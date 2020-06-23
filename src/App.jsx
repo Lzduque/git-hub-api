3e78020f637f3f5b0a33f608b06c04af0d8a03fa
Leticia <lzduque@hotmail.com>
2020-06-23 17:53:26 -0400

    2columns adjustments, no navbar

--- src/App.jsx
+++ src/App.jsx
@@ -31,13 +31,7 @@ function App() {
         <h1> Header </h1>
         <p> Resize the browser window to see the responsive effect.
         </p>
-      </div>
-      <div className="topnav">
-        <a href="#"> Link </a>
-        <a href="#"> Link </a>
-        <a href="#"> Link </a>
-      </div>
-  `
+      </div>  `
       <div className="row">
         <div className="column">
           <h2> Column </h2>
@@ -48,10 +42,6 @@ function App() {
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Maecenas sit amet pretium urna.Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit.Praesent scelerisque tortor sed accumsan convallis.</p>
       </div>
 
-      <div className="column">
-        <h2> Column </h2>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Maecenas sit amet pretium urna.Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique.Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit.Praesent scelerisque tortor sed accumsan convallis.</p>
-      </div>
     </div>
   </div>
   );
